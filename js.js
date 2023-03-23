2fdefd71fba87fd7ec94ca2604389329bbc44a29
luckypabalinas <113162222+luckypabalinas@users.noreply.github.com>
2023-03-23 15:19:16 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,42 @@
+const menu = document.querySelector(".hamburger");
+const menuLink = document.querySelector(".nav-menu");
+const navLogo = document.querySelector(".nav-logo");
+
+const hamburger = () => {
+    menu.classList.toggle('is-active')
+    menuLink.classList.toggle('active')
+};
+
+menu.addEventListener('click', hamburger);
+
+const hideHamburger = () => {
+    const menuBars = document.querySelector('.is-active');
+    if (window.innerWidth <= 768 && menuBars) {
+        menu.classList.toggle('is-active');
+        menuLink.classList.remove('active');
+    }
+}
+
+menuLink.addEventListener('click', hideHamburger);
+navLogo.addEventListener('click', hideHamburger);
+
+const icons = document.querySelectorAll(".icon");
+const bg = document.querySelector("body");
+const dark = document.querySelector('.fa-moon')
+const light = document.querySelector('.fa-sun')
+const btn = document.querySelector('.dark-mode')
+
+const chngBg = () => {
+    bg.classList.toggle('night');
+    icons.forEach(icon => icon.classList.toggle('asd'));
+    if (bg.classList.contains('night')) {
+        dark.style.display = 'none';
+        light.style.display = 'block';
+    } else {
+        light.style.display = 'none';
+        dark.style.display = 'block';
+    }
+};
+
+
+btn.addEventListener('click', chngBg);
